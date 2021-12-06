e2cbdf79c80dab0cc837246ae5b1e072f6f97a4f
Caio Cesar <caiocsmeneghelli@outlook.com>
2021-12-06 14:10:51 -0300

    Mudanças dentro do controller e dentro das models.

---
+++ Migrations/202112061426219_AddMembershipType.cs
@@ -0,0 +1,34 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MemberShipTypes",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        SignUpFee = c.Short(nullable: false),
+                        DurationInMonths = c.Byte(nullable: false),
+                        DiscountRate = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Customers", "MemberShipTypesId", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Customers", "MemberShipTypesId");
+            AddForeignKey("dbo.Customers", "MemberShipTypesId", "dbo.MemberShipTypes", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "MemberShipTypesId", "dbo.MemberShipTypes");
+            DropIndex("dbo.Customers", new[] { "MemberShipTypesId" });
+            DropColumn("dbo.Customers", "MemberShipTypesId");
+            DropTable("dbo.MemberShipTypes");
+        }
+    }
+}
