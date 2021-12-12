35949050f0ae11ef37e758b0a0e0624db3553b0b
Caio Cesar <caiocsmeneghelli@outlook.com>
2021-12-12 11:24:50 -0300

    Add Name field in Membershiptype.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,6 +10,30 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.ToList();
+            return View(movies);
+        }
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
