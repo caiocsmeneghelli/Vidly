13cb82f869433b44cea94ef6e263e666721a9b75
Caio Cesar <caiocsmeneghelli@outlook.com>
2022-01-07 17:17:47 -0300

    Add usuarios e roles.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Vidly.Models;
@@ -155,6 +156,15 @@ namespace Vidly.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    // Como não foi possivel gerar uma migration para incluir usuarios e roles
+                    // Criar um user Chamado Guest
+                    // E usar o codigo abaixo para criar o usuario Admin e a Role CanManageMovies
+                    // temp code
+                    //var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    //var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    //await roleManager.CreateAsync(new IdentityRole("CanManageMovies"));
+                    //await UserManager.AddToRoleAsync(user.Id, "CanManageMovies");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
