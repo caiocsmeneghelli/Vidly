ec3216c83c8a82c07f033ddb0cb0c3e259451201
Caio Cesar <caiocsmeneghelli@outlook.com>
2022-01-30 19:49:01 -0300

    Output caching.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Vidly.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        //[OutputCache(Duration = 50, Location = OutputCacheLocation.Server, VaryByParam = "*")]
+        [OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)] // Para desabilitar o caching.
         public ActionResult Index()
         {
             return View();
