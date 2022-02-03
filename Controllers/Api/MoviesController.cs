9000f011304d6b196e69e966421fcbd1b330983a
Caio Cesar <caiocsmeneghelli@outlook.com>
2022-02-02 21:13:43 -0300

    Submit nos novos Rental.

--- Controllers/Api/MoviesController.cs
+++ Controllers/Api/MoviesController.cs
@@ -20,13 +20,20 @@ namespace Vidly.Controllers.Api
         }
 
         // Get /api/movies
-        public IEnumerable<MovieDto> GetMovies()
+        public IHttpActionResult GetMovies(string query = null)
         {
-            return _context.Movies
-                .Include(m => m.Genre)
+            var moviesQuery = _context.Movies
+                .Where(m => m.NumberAvailable > 0)
+                .Include(m => m.Genre);
+            if (!String.IsNullOrWhiteSpace(query))
+                moviesQuery = moviesQuery
+                    .Where(m => m.Name.Contains(query));
+            var moviesDto = moviesQuery
                 .ToList()
                 .Select(Mapper.Map<Movie, MovieDto>);
+            return Ok(moviesDto);
         }
+        
 
         // Get /api/movies/1
         public MovieDto GetMovie(int id)
